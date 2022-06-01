ed827d5726c0783ffceaa48a944aadf636e769ff
artsiom.statuta <arstiom.statuta@ibm.com>
2022-06-01 16:33:08 +0300

    up feat: add hash task

--- src/hash/calcHash.js
+++ src/hash/calcHash.js
@@ -1,3 +1,26 @@
+import path from "path";
+import fs from "fs";
+import crypto from "crypto";
+
 export const calculateHash = async () => {
-    // Write your code here 
+  try {
+    const __dirname = path.resolve();
+    const filePath = path.join(
+      `${__dirname}/src/hash`,
+      "files",
+      "fileToCalculateHashFor.txt"
+    );
+    const hash = crypto.createHash("sha256");
+    const data = hash.update("", "utf-8");
+    const dataHex = data.digest("hex");
+    fs.writeFile(filePath, dataHex, function (err) {
+      if (err) throw err;
+    });
+    console.log(dataHex);
+    return dataHex;
+  } catch (e) {
+    throw e;
+  }
 };
+
+calculateHash();
