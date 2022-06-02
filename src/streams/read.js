8e5e48103ffb289408440278d4866d3ee8ac594a
artsiom.statuta <arstiom.statuta@ibm.com>
2022-06-02 19:07:36 +0300

    fea: add steams

--- src/streams/read.js
+++ src/streams/read.js
@@ -1,3 +1,15 @@
+import fs from "fs";
+import path from "path";
 export const read = async () => {
-    // Write your code here 
+  const __dirname = path.resolve();
+  const filePath = path.join(__dirname, "src/streams/files", "fileToRead.txt");
+  const stream = new fs.ReadStream(filePath);
+  stream.on("readable", async () => {
+    const data = await stream.read();
+    if (data) {
+      const buffered = data.toString();
+      process.stdout.write(`${buffered}\n`);
+    }
+  });
 };
+read();
--- src/streams/write.js
+++ src/streams/write.js
@@ -1,3 +1,13 @@
+import fs from "fs";
+import path from "path";
 export const write = async () => {
-    // Write your code here 
+  const __dirname = path.resolve();
+  const filePath = path.join(__dirname, "src/streams/files", "fileToWrite.txt");
+  const stream = new fs.WriteStream(filePath, { encoding: "utf-8" });
+  process.stdin.on("data", (data) => {
+    stream.write(data.toString());
+  });
+  process.on("SIGINT", process.exit);
 };
+
+write();
